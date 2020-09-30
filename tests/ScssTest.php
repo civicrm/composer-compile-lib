fa4a2c50233ebbd9e70064b9d022e79c4b099dbf
Tim Otten <totten@civicrm.org>
2020-09-29 18:42:38 -0700

    ScssTest - Add globMap example

--- tests/ScssTest.php
+++ tests/ScssTest.php
@@ -11,28 +11,53 @@ class ScssTest extends \PHPUnit\Framework\TestCase {
     ]);
 
     $this->assertTrue(file_exists($this->exampleDir('output.css')));
-
-    $actual = \CCL\cat($this->exampleDir('output.css'));
-    $actualMin = \CCL\cat($this->exampleDir('output.min.css'));
-    $expected = \CCL\cat($this->exampleDir('expected.css'));
-
-    $normalize = function ($css) {
-      return trim(preg_replace(';\s+;', ' ', $css));
-    };
-    $this->assertEquals($normalize($expected), $normalize($actual));
-
-    $strip = function ($css) {
-      $css = preg_replace(';\s+;', '', $css);
-      $css = str_replace(';}', '}', $css);
-      return $css;
-    };
-    $this->assertEquals($strip($expected), $strip($actualMin));
+    $this->assertSameCss($this->exampleDir('expected.css'), $this->exampleDir('output.css'));
+    $this->assertSameMinCss($this->exampleDir('expected.css'), $this->exampleDir('output.min.css'));
   }
+
+  public function testScssGlobMap() {
+    \CCL\remove($this->exampleDir('output.css'));
+    \CCL\Tasks::scss([
+      'scss-files' => \CCL\globMap($this->exampleDir('*.scss'), $this->exampleDir('dist/#1.css')),
+      'scss-imports' => ['tests/examples/scss'],
+    ]);
+
+    $this->assertTrue(file_exists($this->exampleDir('dist/main.css')));
+    $this->assertSameCss($this->exampleDir('expected.css'), $this->exampleDir('dist/main.css'));
+    $this->assertSameMinCss($this->exampleDir('expected.css'), $this->exampleDir('dist/main.min.css'));
+  }
+
 
   protected function exampleDir($name = NULL) {
     // For this task, file paths are relative to cwd.
     $base = 'tests/examples/scss';
     return $name ? "$base/$name" : $base;
+  }
+
+  /**
+   * @param $expectedCssFile
+   * @param $outputCssFile
+   */
+  public function assertSameCss($expectedCssFile, $outputCssFile) {
+    $normalize = function ($css) {
+      return trim(preg_replace(';\s+;', ' ', $css));
+    };
+    $this->assertEquals($normalize(\CCL\cat($expectedCssFile)),
+      $normalize(\CCL\cat($outputCssFile)));
+  }
+
+  /**
+   * @param $expectedCssFile
+   * @param $outputMinCssFile
+   */
+  public function assertSameMinCss($expectedCssFile, $outputMinCssFile) {
+    $strip = function ($css) {
+      $css = preg_replace(';\s+;', '', $css);
+      $css = str_replace(';}', '}', $css);
+      return $css;
+    };
+    $this->assertEquals($strip(\CCL\cat($expectedCssFile)),
+      $strip(\CCL\cat($outputMinCssFile)));
   }
 
 }
